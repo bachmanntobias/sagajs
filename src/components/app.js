361a3dcb4b540779fd077126eb38765f82e5e1f1
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-07 16:31:08 +0100

    commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import {Rating} from 'semantic-ui-react';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="App">
+        <div className=""App-header>
+          <h2>head</h2>
+          <p>toes</p>
+        </div>
+      </div>
     );
   }
 }
